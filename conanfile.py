e79c9312463c7e587cfcc47db1a7b07f93ed166a
Quentin Chateau <quentin.chateau@gmail.com>
2019-12-14 18:40:09 +0100

    test boost and msgpack versions

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,10 @@ class PackioConan(ConanFile):
     topics = ("rpc", "msgpack")
     exports_sources = "include/*"
     no_copy_source = True
-    requires = ["msgpack/3.2.0@bincrafters/stable", "boost/1.71.0@conan/stable"]
+    requires = [
+        "msgpack/[>=3.0]@bincrafters/stable",
+        "boost/[>=1.70]@conan/stable",
+    ]
     build_policy = "missing"
 
     def package(self):
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,6 +7,14 @@ class PackioConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     requires = ["gtest/1.8.1@bincrafters/stable", "spdlog/1.4.2@bincrafters/stable"]
+    options = {"boost": "ANY", "msgpack": "ANY"}
+    default_options = {"boost": None, "msgpack": None}
+
+    def requirements(self):
+        if self.options.boost:
+            self.requires("boost/{}@conan/stable".format(self.options.boost))
+        if self.options.msgpack:
+            self.requires("msgpack/{}@bincrafters/stable".format(self.options.msgpack))
 
     def build(self):
         cmake = CMake(self)
@@ -16,5 +24,5 @@ class PackioConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            self.run(".%sbasic" % os.sep)
-            self.run(".%smt" % os.sep)
+            self.run(".{}basic".format(os.sep))
+            self.run(".{}mt".format(os.sep))
