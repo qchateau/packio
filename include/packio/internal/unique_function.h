4e4cc110c2144c65fedebe898f51990f6b91bcb6
Quentin Chateau <quentin.chateau@gmail.com>
2020-01-04 11:55:16 +0100

    added unique_function

---
+++ include/packio/internal/unique_function.h
@@ -0,0 +1,95 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef PACKIO_UNIQUE_FUNCTION_H
+#define PACKIO_UNIQUE_FUNCTION_H
+
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+namespace packio {
+namespace internal {
+
+template <typename T>
+class unique_function : public std::function<T> {
+    template <typename Fn, typename En = void>
+    struct wrapper {
+    };
+
+    // specialization for CopyConstructible Fn
+    template <typename Fn>
+    struct wrapper<Fn, std::enable_if_t<std::is_copy_constructible<Fn>::value>> {
+        Fn fn;
+
+        template <typename... Args>
+        auto operator()(Args&&... args)
+        {
+            return fn(std::forward<Args>(args)...);
+        }
+    };
+
+    // specialization for MoveConstructible-only Fn
+    template <typename Fn>
+    struct wrapper<
+        Fn,
+        std::enable_if_t<
+            !std::is_copy_constructible<Fn>::value
+            && std::is_move_constructible<Fn>::value>> {
+        Fn fn;
+
+        wrapper(Fn&& fn) : fn(std::forward<Fn>(fn)) {}
+
+        wrapper(wrapper&&) = default;
+        wrapper& operator=(wrapper&&) = default;
+
+        // these two functions are instantiated by std::function and are never called
+        wrapper(const wrapper& rhs) : fn(const_cast<Fn&&>(rhs.fn))
+        {
+            // hack to initialize fn for non-DefaultContructible types
+            std::abort();
+        }
+        wrapper& operator=(wrapper&) { std::abort(); }
+
+        template <typename... Args>
+        auto operator()(Args&&... args)
+        {
+            return fn(std::forward<Args>(args)...);
+        }
+    };
+
+    using base = std::function<T>;
+
+public:
+    unique_function() noexcept = default;
+    unique_function(std::nullptr_t) noexcept : base(nullptr) {}
+
+    template <typename Fn>
+    unique_function(Fn&& f) : base(wrapper<Fn>{std::forward<Fn>(f)})
+    {
+    }
+
+    unique_function(unique_function&&) = default;
+    unique_function& operator=(unique_function&&) = default;
+
+    unique_function& operator=(std::nullptr_t)
+    {
+        base::operator=(nullptr);
+        return *this;
+    }
+
+    template <typename Fn>
+    unique_function& operator=(Fn&& f)
+    {
+        base::operator=(wrapper<Fn>{std::forward<Fn>(f)});
+        return *this;
+    }
+
+    using base::operator();
+};
+
+} // internal
+} // packio
+
+#endif // PACKIO_UNIQUE_FUNCTION_H
