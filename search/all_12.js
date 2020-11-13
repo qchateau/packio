8a9eaa8952b6433b71deed1745342501804a0992
qchateau <quentin.chateau@gmail.com>
2020-11-13 12:11:46 +0000

    Deploying to gh-pages from  @ 62e0e68a05d9fd16a78969e9dbccd7e1c03fcbfa 🚀

--- search/all_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7ecompletion_5fhandler_67',['~completion_handler',['../classpackio_1_1completion__handler.html#adfb61907d9efb108be79040140ebbcd9',1,'packio::completion_handler']]]
-];
--- search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['error_132',['error',['../namespacepackio.html#ad0fad01adc607b1ef672314d1ed01809',1,'packio']]]
-];
