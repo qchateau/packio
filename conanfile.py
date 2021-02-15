56dd1eb69176c6f5ce476df327b1de4c37cf8c81
Quentin Chateau <quentin.chateau@gmail.com>
2021-02-15 21:26:24 +0100

    packio: bump version

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class PackioConan(ConanFile):
     name = "packio"
-    version = "2.1.0"
+    version = "2.2.0"
     license = "MPL-2.0"
     author = "Quentin Chateau <quentin.chateau@gmail.com>"
     url = "https://github.com/qchateau/packio"
